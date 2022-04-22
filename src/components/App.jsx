1c96dfe91731a9865559bf99ccbd24cd510ab6a6
eijjeka <eijjeka@gmail.com>
2022-04-22 20:06:50 +0300

    add files

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div class={{scroll123455}}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
